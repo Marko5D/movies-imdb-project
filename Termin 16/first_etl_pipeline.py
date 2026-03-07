c218c53993b43f20f1a527ece2d3cac86b0c392b
Marko Drazic <alek321@mts.rs>
2026-03-07 20:28:32 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -9,4 +9,4 @@ for i in range(df.shape[0]):
 
         df.loc[i, 'rental_date'] = pd.to_datetime(value, format='%d.%m.%Y', errors='raise')
 
-        print(i, type(value).__name__, value)
+        print(df.dtypes)
