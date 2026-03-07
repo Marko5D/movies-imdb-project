ce91573b1a096fe7e68dbdfd9d2d973353dcad68
Marko Drazic <alek321@mts.rs>
2026-03-07 20:34:18 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -10,3 +10,9 @@ for i in range(df.shape[0]):
         df.loc[i, 'rental_date'] = pd.to_datetime(value, format='%d.%m.%Y', errors='raise')
 
         print(df.dtypes)
+
+        for i in range(df.shape[0]):
+            value = df.loc[i, 'rental_date']
+
+            if(type(value).__name__ != 'datetime' and not(pd.isna(value))):
+                print(i, type(value).__name__, value)
