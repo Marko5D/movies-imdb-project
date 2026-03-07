a0d036a9fb3822711ebf2a4eb0fec8e8c276419c
Marko Drazic <alek321@mts.rs>
2026-03-07 21:02:15 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -2,6 +2,18 @@ import pandas as pd
 
 df = pd.read_excel('users_rentals_v2.xlsx')
 
+for i in range(df.shape[0]):
+    value = df.loc[i, 'rental_date']
+
+    if(type(value).__name__ != 'datetime' and not(pd.isna(value))):
+
+        df.loc[i, 'rental_date'] = pd.to_datetime(value, format='%d.%m.%Y', errors='raise')
+
 df['rental_date'] = pd.to_datetime(df['rental_date'], errors='raise')
 
 print(df.dtypes)
+
+today = pd.Timestamp.today()
+interval = today - df.loc[1, 'rental_date']
+
+print(interval.days > 31)
