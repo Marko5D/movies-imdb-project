6ea07ccbeaf15f735792d6c499ae567ef320b6ed
Marko Drazic <alek321@mts.rs>
2026-03-07 19:40:23 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_excel('users_rentals_v2.xlsx')
 
-for i in range(df.shape[0]):
-    value = df.loc[i, 'total_rentals']
-    print(i, type(value).__name__, value)
+df['total_rentals'] = df['total_rentals'].astype('float')
+
+print(df.dtypes)
