fcf75d23b7b4f21bfd464fc0380b77ba0d613669
Marko Drazic <alek321@mts.rs>
2026-03-07 19:27:08 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 df = pd.read_excel('users_rentals_v2.xlsx')
 
-print(df.info())
+for i in range(df.shape[0]):
+    value = df.loc[i, 'total_rentals']
+    print(i, type(value).__name__, value)
