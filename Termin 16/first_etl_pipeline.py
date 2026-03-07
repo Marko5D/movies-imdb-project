f4fd95bc9ca411a69093118bb81ccef659ddf680
Marko Drazic <alek321@mts.rs>
2026-03-07 19:43:33 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_excel('users_rentals_v2.xlsx')
 
-df['total_rentals'] = df['total_rentals'].astype('float')
+df['total_rentals'] = pd.to_numeric(df['total_rentals'], errors='coerce')
 
 print(df.dtypes)
