81d0b5c4b85a4ac5fa7f14ec537d8bc8ad6b07f1
Marko Drazic <alek321@mts.rs>
2026-03-07 20:45:04 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -2,17 +2,6 @@ import pandas as pd
 
 df = pd.read_excel('users_rentals_v2.xlsx')
 
-for i in range(df.shape[0]):
-    value = df.loc[i, 'rental_date']
+df['rental_date'] = pd.to_datetime(df['rental_date'], errors='raise')
 
-    if(type(value).__name__ != 'datetime' and not(pd.isna(value))):
-
-        df.loc[i, 'rental_date'] = pd.to_datetime(value, format='%d.%m.%Y', errors='raise')
-
-        print(df.dtypes)
-
-        for i in range(df.shape[0]):
-            value = df.loc[i, 'rental_date']
-
-            if(type(value).__name__ != 'datetime' and not(pd.isna(value))):
-                print(i, type(value).__name__, value)
+print(df.dtypes)
