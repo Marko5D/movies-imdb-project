03aeb62577353eba674c2df8aa1b35645283f49a
Marko Drazic <alek321@mts.rs>
2026-03-07 21:34:25 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_excel('users_rentals_v2.xlsx')
+df = pd.read_excel('users_rentals_v2.xlsx', engine='openpyxl')
 
 for i in range(df.shape[0]):
     value = df.loc[i, 'rental_date']
@@ -14,5 +14,7 @@ df['rental_date'] = pd.to_datetime(df['rental_date'], errors='raise')
 
 mask = (pd.Timestamp.today() - df['rental_date']) > pd.Timedelta(days=31)
 new_df = df[mask]
+
+print(new_df)
 
       
--- Termin 16/main.py
+++ Termin 16/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 data = pd.Series([10, 20, 30, 40, 50])
+data2 = pd.Series([10, 20, 30, 40, 50])
 
-result = data * 2
+result = data * data2
 
 print(result)
