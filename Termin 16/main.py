f2af89ccd87655c91239111bd5530d456924112c
Marko Drazic <alek321@mts.rs>
2026-03-07 21:44:56 +0100

    Auto-sync: file changes

--- Termin 16/main.py
+++ Termin 16/main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-data = pd.Series([10, 20, 30, 40, 50])
-data2 = pd.Series([10, 20, 30, 40, 50, 60])
+data1 = pd.DataFrame([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+data2 = pd.DataFrame([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 
-result = data * data2
+result = data1 * data2
 
 print(result)
