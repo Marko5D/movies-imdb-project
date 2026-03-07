7a47db638bd2bcd828b926038e4eae4830aa9a1d
Marko Drazic <alek321@mts.rs>
2026-03-07 18:55:51 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -2,7 +2,4 @@ import pandas as pd
 
 df = pd.read_excel('users_rentals_v2.xlsx')
 
-today = pd.Timestamp.today()
-interval = today - df.loc(1, 'rental_date')
-
-print(interval.days > 31)
+print(df.dtypes)
