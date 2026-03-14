bed364af059afa9ae2cdfb3dc6b177da010bc1e7
Marko Drazic <alek321@mts.rs>
2026-03-14 15:28:43 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -1,31 +1,42 @@
 import pandas as pd
 
 #extract
-df = pd.read_excel('users_rentals_v2.xlsx', engine='openpyxl')
+def extract_data(file_name):
+    return pd.read_excel(file_name, engine='openpyxl')
 
 #clean
-for i in range(df.shape[0]):
-    value = df.loc[i, 'rental_date']
+def clean_data(df):
+    for i in range(df.shape[0]):
+        value = df.loc[i, 'rental_date']
 
-    if(type(value).__name__ != 'datetime' and not(pd.isna(value))):
+        if(type(value).__name__ != 'datetime' and not(pd.isna(value))):
 
-        df.loc[i, 'rental_date'] = pd.to_datetime(value, format='%d.%m.%Y', errors='raise')
+            df.loc[i, 'rental_date'] = pd.to_datetime(value, format='%d.%m.%Y', errors='raise')
 
-df['rental_date'] = pd.to_datetime(df['rental_date'], errors='raise')
+    df['rental_date'] = pd.to_datetime(df['rental_date'], errors='raise')
+    return df
 
 #filter data
-mask = (pd.Timestamp.today() - df['rental_date']) > pd.Timedelta(days=31)
-df_filtered = df[mask]
+def filter_data(df):
+    mask = (pd.Timestamp.today() - df['rental_date']) > pd.Timedelta(days=31)
+    return df [mask]
 
 #sort data
-df_filtered_sorted = df_filtered.sort_values(by='rental_date', ascending=True)
-
+def sort_data(df):
+    return df.sort_values(by='rental_date', ascending=True)
+    
 #add new column
-df_filtered_sorted['overdue_days'] = (pd.Timestamp.today() - df_filtered_sorted ['rental_date']).dt.days - 31 
+def add_new_column(df):
+    df['overdue_days'] = (pd.Timestamp.today() - df['rental_date']).dt.days - 31 
+    return df
 
 #remove columns
-df_filtered_sorted_cleaned = df_filtered_sorted.drop(['address', 'gender', 'city', 'active'], axis=1) 
+def remove_columns(df):
+    return df.drop(columns=['address', 'gender', 'city', 'active']) 
 
 #load data
-df_filtered_sorted_cleaned.to_excel('overdue_users.xlsx', index=False)
-      
+def load_data(df):
+    df.to_excel('overdue_users.xlsx', index=False)
+    return df
+
+df = extract_data('users_rentals_v2.xlsx').pipe(clean_data).pipe(filter_data).pipe(sort_data).pipe(add_new_column).pipe(remove_columns).pipe(load_data)
