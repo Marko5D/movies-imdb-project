0cafde1474ad8e65b1c6f545c9316a9d123f283d
Marko Drazic <alek321@mts.rs>
2026-03-14 12:58:56 +0100

    Auto-sync: file changes

--- Termin 16/first_etl_pipeline.py
+++ Termin 16/first_etl_pipeline.py
@@ -15,6 +15,10 @@ df['rental_date'] = pd.to_datetime(df['rental_date'], errors='raise')
 mask = (pd.Timestamp.today() - df['rental_date']) > pd.Timedelta(days=31)
 new_df = df[mask]
 
+
+new_df.sort_values(by='rental_date')
+
+
 print(new_df)
 
       
