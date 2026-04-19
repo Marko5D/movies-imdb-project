73e2db47dd05f2aae35388a54a08e4cf0f5a5e30
Marko Drazic <alek321@mts.rs>
2026-04-19 15:07:21 +0200

    Auto-sync: file changes

--- Zadaci/movies_IMDb/main.py
+++ Zadaci/movies_IMDb/main.py
@@ -10,6 +10,7 @@ def load_movies(file_path):
 
     with open(file_path, mode="r", encoding="utf-8-sig") as file:
         reader = csv.DictReader(file)
+
         for row in reader:
             movies.append(row)
 
@@ -23,7 +24,18 @@ def get_movie_data(title, year):
 
     data = response.json()
 
-    return data
+    if data.get("Response") == "True":
+        return{
+            "imdb_rating": data.get("imdbRating"),
+            "actors": data.get("Actors"),
+            "imdb_votes": data.get("imdbVotes")
+        }
+    else:
+        return{
+            "imdb_rating": "N/A",
+            "actors": "N/A",
+            "imdb_votes": "N/A"
+        }
 # Ucitavanje filmova iz CSV fajla
 movies = load_movies("movies.csv")
 
@@ -31,13 +43,18 @@ print("Broj ucitanih filmova:", len(movies))
 print("Prvi red:")
 print(movies[0])
 
+# Uzimamo prvi film iz liste
 first_movie = movies[0]
+
 # poziv funkcije za dobijanje podataka sa API-ja
 api_data = get_movie_data(
     first_movie["title"],
     first_movie["release_year"]
 )
 
+# Dodavanje novih podataka u prvi film
+first_movie.update(api_data)
+
 # Ispis podataka koje vraca API
-print("\nAPI PODACI:")
-print(api_data)
+print("\nPRVI FILM NAKON DODAVANJA API PODATAKA:")
+print(first_movie)
