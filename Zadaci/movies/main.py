e7a693be226a6875c14f0280e4e69449822645df
Marko Drazic <alek321@mts.rs>
2026-04-19 13:32:58 +0200

    Auto-sync: file changes

--- Zadaci/movies/main.py
+++ Zadaci/movies/main.py
@@ -2,10 +2,11 @@ import pandas as pd
 
 df = pd.read_csv("DMSP4-Task_02-movies.csv")
 
-usa_df = df[df["country"].str.contains("USA", na=False)]
+df["budget"] = pd.to_numeric(df["budget"], errors="coerce")
+df["box_office"] = pd.to_numeric(df["box_office"], errors="coerce")
+
+usa_df = df[df["country"].str.contains("USA", na=False)].copy()
+
+usa_df["balance"] = usa_df["box_office"] - usa_df["budget"]
 
 print(usa_df.head())
-
-print(df.head())
-
-print(df["country"].unique())
