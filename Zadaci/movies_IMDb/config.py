d6a1f5d6137486a8ec9434253de21d681a72fdd1
Marko Drazic <alek321@mts.rs>
2026-04-19 14:59:44 +0200

    Auto-sync: file changes

---
+++ Zadaci/movies_IMDb/config.py
@@ -0,0 +1,2 @@
+# API kljuc za pristup OMDb servisu
+API_KEY = "1f02860a"
--- Zadaci/movies_IMDb/main.py
+++ Zadaci/movies_IMDb/main.py
@@ -1,17 +1,43 @@
+# Uvoz potrebhih biblioteka
 import csv
+import requests
+from config import API_KEY
 
+
+# Ucitavanje CSV fajla
 def load_movies(file_path):
     movies = []
 
-    with open(file_path, mode="r", encoding="utf-8") as file:
+    with open(file_path, mode="r", encoding="utf-8-sig") as file:
         reader = csv.DictReader(file)
         for row in reader:
             movies.append(row)
 
     return movies
 
+# Funkcija za dobijanje podataka sa OMDb API-ja
+def get_movie_data(title, year):
+    url= f"http://www.omdbapi.com/?t={title}&y={year}&apikey={API_KEY}"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    return data
+# Ucitavanje filmova iz CSV fajla
 movies = load_movies("movies.csv")
 
 print("Broj ucitanih filmova:", len(movies))
 print("Prvi red:")
 print(movies[0])
+
+first_movie = movies[0]
+# poziv funkcije za dobijanje podataka sa API-ja
+api_data = get_movie_data(
+    first_movie["title"],
+    first_movie["release_year"]
+)
+
+# Ispis podataka koje vraca API
+print("\nAPI PODACI:")
+print(api_data)
