501785a5563a161bbd9871163a727c48cebdb146
Marko5D <alek321@mts.rs>
2026-04-26 22:00:35 +0200

    Auto-sync: file changes

--- Zadaci/Marko_Drazic_Task6/zadaci.py
+++ Zadaci/Marko_Drazic_Task6/zadaci.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import pandas as pd
 
 conn = mysql.connector.connect(
     host="localhost",
@@ -12,11 +13,20 @@ cursor = conn.cursor()
 cursor.execute("""
 Select g.name AS genre, m.budget
 FROM movie m
-JOIN movie_genre mg ON m.id = mg.movie_id
-JOIN genre g ON mg.genre_id = g.id
+JOIN movie_genre mg ON m.movie_id = mg.movie_id
+JOIN genre g ON mg.genre_id = g.genre_id
 WHERE m.budget IS NOT NULL;
 """)
 
 result = cursor.fetchall()
 
 print(result)
+
+df = pd.DataFrame(result, columns=["genre", "budget"])
+
+print(df.head())
+
+avg_budget = df.groupby("genre")["budget"].mean()
+
+print(avg_budget)
+print(df.describe())
