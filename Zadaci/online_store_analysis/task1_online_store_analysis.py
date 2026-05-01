e89c7a2425512d24ebf0da5dd56d13f589c184bb
Marko5D <alek321@mts.rs>
2026-05-01 13:01:32 +0200

    Auto-sync: file changes

--- Zadaci/online_store_analysis/task1_online_store_analysis.py
+++ Zadaci/online_store_analysis/task1_online_store_analysis.py
@@ -6,5 +6,11 @@ df = pd.read_csv("online_store_data.csv")
 # Prikaz prvih 5 (da proverimo da li radi)
 print(df.head())
 
-# Ukupan broj proizvoda
+# 1. Ukupan broj proizvoda
 print("\nUkupan broj proizvoda:", df.shape[0])
+
+# 2. Najprodavaniji proizvod
+najprodavaniji = df.sort_values(by="quantity_sold", ascending=False).iloc[0]
+
+print("\nNajprodavaniji proizvod:")
+print(najprodavaniji)
